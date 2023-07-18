3565d11f8cbd93ea87e85456113352d2278945b6
mynamedust <mynamedust@gmail.com>
2023-07-18 21:33:40 +0400

    finish ex02/day01

--- day01/ex02/fileDumpCompare.go
+++ day01/ex02/fileDumpCompare.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"compress/gzip"
+	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -21,12 +18,7 @@ func main() {
 	if oldFile == nil || newFile == nil {
 		return
 	}
-	zipNew, zipOld := dataCompress(newFile), dataCompress(oldFile)
-	if zipNew == nil || zipOld == nil {
-		fmt.Println("Data compressing error.")
-		return
-	}
-	zipDataCompare(zipNew, zipOld)
+	dataCompare(oldFile, newFile)
 }
 
 func checkFile(path string) *os.File {
@@ -43,40 +35,21 @@ func checkFile(path string) *os.File {
 	return file
 }
 
-func dataCompress(file *os.File) (data []byte) {
-	var compressData bytes.Buffer
-	gzipWritter := gzip.NewWriter(&compressData)
-	defer gzipWritter.Close()
-	_, err := io.Copy(gzipWritter, file)
-	if err != nil {
-		return nil
+func dataCompare(oldFile, newFile *os.File) {
+	oldData := make(map[string]struct{})
+	rOld := bufio.NewScanner(oldFile)
+	rNew := bufio.NewScanner(newFile)
+	for rOld.Scan() {
+		oldData[rOld.Text()] = struct{}{}
 	}
-	return compressData.Bytes()
-}
-
-func zipDataCompare(bytes1, bytes2 []byte) {
-	lines1 := zipDataSplit(bytes1)
-	lines2 := zipDataSplit(bytes2)
-	map1 := make(map[string]bool, len(lines1))
-	for _, line := range lines1 {
-		map1[line] = false
-	}
-	for _, line := range lines2 {
-		if _, ok := map1[line]; !ok {
-			fmt.Printf("ADDED %s\n", line)
+	for rNew.Scan() {
+		if _, ok := oldData[rNew.Text()]; !ok {
+			fmt.Printf("ADDED %s\n", rNew.Text())
 			continue
 		}
-		map1[line] = true
+		delete(oldData, rNew.Text())
 	}
-	for _, line := range lines1 {
-		if map1[line] == false {
-			fmt.Printf("REMOVED %s\n", line)
-		}
+	for key := range oldData {
+		fmt.Printf("REMOVED %s\n", key)
 	}
-}
-
-func zipDataSplit(data []byte) []string {
-	str := string(data)
-	arr := strings.Split(str, "\n")
-	return arr
 }
