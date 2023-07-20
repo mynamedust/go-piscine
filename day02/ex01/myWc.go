8149ec48e973940957aedb829b8267108d5c7ca3
Albert Melikyan <almeliky@c2r13s1.42yerevan.am>
2023-07-20 17:46:12 +0400

    add lineCounter and wordsCounter

--- day02/ex01/myWc.go
+++ day02/ex01/myWc.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 )
 
@@ -25,7 +28,7 @@ func main() {
 	countItems(counter, args)
 }
 
-func selectCounter(w, l, m bool) func(args string, wg *sync.WaitGroup) int {
+func selectCounter(w, l, m bool) func(args string, wg *sync.WaitGroup) {
 	switch {
 	case w:
 		return wordCounter
@@ -38,7 +41,7 @@ func selectCounter(w, l, m bool) func(args string, wg *sync.WaitGroup) int {
 	}
 }
 
-func countItems(counter func(string, *sync.WaitGroup) int, args []string) {
+func countItems(counter func(string, *sync.WaitGroup), args []string) {
 	wg := new(sync.WaitGroup)
 	for _, name := range args {
 		if !validName(name) {
@@ -53,23 +56,66 @@ func countItems(counter func(string, *sync.WaitGroup) int, args []string) {
 func validName(name string) bool {
 	ext := filepath.Ext(name)
 	if ext != ".txt" {
-		fmt.Println("Error.Wrong file type.")
+		fmt.Printf("Error reading file \"%s\".Wrong file type.\n", name)
 		return false
 	}
 	return true
 }
 
-func wordCounter(file string, wg *sync.WaitGroup) (count int) {
+func wordCounter(fileName string, wg *sync.WaitGroup) {
+	var count int
 	defer wg.Done()
-	fmt.Println(file)
+	data := openRead(fileName)
+	if data == nil {
+		return
+	}
+	words := strings.Split(string(data), " ")
+	fmt.Println(len(words))
+	for _, elem := range words {
+		if strings.Compare(elem, " ") != 0 {
+			fmt.Println(elem, "!=", elem != " ")
+			count++
+			count += strings.Count(elem, "\n")
+		}
+	}
+	fmt.Println(count)
 }
 
-func lineCounter(file string, wg *sync.WaitGroup) (count int) {
+func lineCounter(fileName string, wg *sync.WaitGroup) {
+	var count int
 	defer wg.Done()
-	fmt.Println(file)
+	data := openRead(fileName)
+	if data == nil {
+		return
+	}
+	if len(data) > 0 {
+		count = 1
+	}
+	for _, elem := range data {
+		if elem == '\n' {
+			count++
+		}
+	}
+	fmt.Println(count)
 }
 
-func charCounter(file string, wg *sync.WaitGroup) (count int) {
+func charCounter(fileName string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println(file)
+	fmt.Println(fileName)
+	return
+}
+
+func openRead(fileName string) []byte {
+	file, err := os.Open(fileName)
+	defer file.Close()
+	if err != nil {
+		fmt.Printf("File \"%s\" opening error: %s", fileName, err)
+		return nil
+	}
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Printf("File \"%s\" reading error: %s", fileName, err)
+		return nil
+	}
+	return data
 }
