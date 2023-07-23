323a89322be218a43ec3acae75891afa107cbf05
mynamedust <mynamedust@gmail.com>
2023-07-23 23:06:28 +0400

    add myRotate.go

--- day02/ex03/myRotate.go
+++ day02/ex03/myRotate.go
@@ -1,7 +1,17 @@
 package main
 
-import ()
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	
+	dir := flag.String("a", ".", "Write valid path for archives.")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("Error. There must be at least one argument.")
+		return
+	}
+	createArchives(*dir, args)
 }
