42463bae6d0ae897e896019a09107aa6670c3b1b
mynamedust <mynamedust@gmail.com>
2023-07-30 23:37:38 +0400

    add index create and mapping

--- day03/ex00/dataToDB.go
+++ day03/ex00/dataToDB.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+
+	//"strings"
 	"github.com/elastic/go-elasticsearch/v8"
 )
 
@@ -27,13 +30,35 @@ func main() {
 		}
 	}
 	cfg := elasticsearch.Config{
-        Addresses: []string{
-            "http://localhost:9200",
-        },
+		Addresses: []string{
+			"http://localhost:9200",
+		},
 	}
 	es, err := elasticsearch.NewClient(cfg)
 	if err != nil {
-  		fmt.Printf("Error creating the client: %s", err)
+		fmt.Printf("Error creating the client: %s", err)
 	}
-	es.Get("places","1")
+	fmt.Println(es)
+	mapping, status := mappingCreate("../materials/schema.json")
+	if status == 1 {
+		return
+	}
+	res, err := es.Indices.Create("places", es.Indices.Create.WithBody(strings.NewReader(mapping)))
+	fmt.Println(res)
+}
+
+func mappingCreate(fileName string) (string, int) {
+	file, err := os.Open(fileName)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("error: file opening failed: ", err)
+		return "", 1
+	}
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("error: file reading failed: ", err)
+		return "", 1
+	}
+	mapping := fmt.Sprintf("{\n\"mappings\": %s}", string(data))
+	return mapping, 0
 }
